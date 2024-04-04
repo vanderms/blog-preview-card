b6e27a56600666d222d0fb6eb0a2c512e9f555ae
Vander Santos <vanderms.84@gmail.com>
2024-04-03 23:14:50 -0300

    feat: blog preview card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,43 @@
-
+<header class="sr-only">
+  <h1>Blog Preview Card</h1>
+</header>
+<main>
+  <div class="w-fit mx-auto">
+    <article
+      class="card max-w-96 mx-6 bg-neutral-000 p-6 my-[9.75rem] rounded-[1.25rem] sm:my-[13.6563rem]"
+    >
+      <figure>
+        <img
+          src="/images/illustration-article.svg"
+          class="rounded-[.625rem] h-[12.5rem] object-fit object-cover"
+          alt=""
+        />
+      </figure>
+      <header class="mt-6">
+        <p
+          class="bg-primary-700 emphasis-sm text-neutral-900 w-fit px-3 py-1 rounded"
+        >
+          <span class="sr-only">Theme: </span>Learning
+        </p>
+        <p class="mt-3 text-neutral-900 body-sm">
+          Published <time datetime="12-21-2023">21 Dec 2023</time>
+        </p>
+        <h2 class="mt-3 heading-md text-neutral-900">HTML & CSS foundations</h2>
+        <p class="mt-3 body-md text-neutral-600">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </header>
+      <footer class="mt-6 flex items-center gap-x-3">
+        <img
+          class="w-8 h-8 rounded-full"
+          src="/images/image-avatar.webp"
+          alt="Greg Hooper"
+        />
+        <p class="emphasis-sm">
+          <span class="sr-only">Posted by</span>Greg Hooper
+        </p>
+      </footer>
+    </article>
+  </div>
+</main>
